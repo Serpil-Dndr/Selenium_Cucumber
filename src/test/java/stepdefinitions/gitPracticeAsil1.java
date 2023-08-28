d9e4ad06313b346a8f1352027ce43ee621e452e7
asil <asiltekelioglu@gmail.com>
2023-08-28 13:33:30 -0700

    wip1

--- src/test/java/stepdefinitions/gitPracticeAsil1.java
+++ src/test/java/stepdefinitions/gitPracticeAsil1.java
@@ -2,6 +2,8 @@ package stepdefinitions;
 
 public class gitPracticeAsil1 {
     public static void main(String[] args) {
+        int serpil = 36;
+        System.out.println("github practice2");
         int asil = 33;
         String asil2 =" github practice";
     }
