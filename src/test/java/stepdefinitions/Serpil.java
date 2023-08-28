9822cf1d51e0d179c0ea49e4d7329ec150d7ef74
Serpil-Dndr <serpil.dndrca@gmail.com>
2023-08-28 16:25:32 -0400

    please conflict

--- src/test/java/stepdefinitions/Serpil.java
+++ src/test/java/stepdefinitions/Serpil.java
@@ -2,6 +2,11 @@ package stepdefinitions;
 
 public class Serpil {
     public static void main(String[] args) {
+
         System.out.println("hello everyone");
+        int asil = 33;
+        String asil2 =" github practice";
+
+
     }
 }
