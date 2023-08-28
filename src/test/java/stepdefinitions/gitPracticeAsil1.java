df6e83acd13625cb8d3e3e5e761240f705190be8
asil <asiltekelioglu@gmail.com>
2023-08-28 13:04:46 -0700

    asil added 1 class

---
+++ src/test/java/stepdefinitions/gitPracticeAsil1.java
@@ -0,0 +1,7 @@
+package stepdefinitions;
+
+public class gitPracticeAsil1 {
+    public static void main(String[] args) {
+        System.out.println("asil branchindem stepdefinition da bir class olusturdum");
+    }
+}
