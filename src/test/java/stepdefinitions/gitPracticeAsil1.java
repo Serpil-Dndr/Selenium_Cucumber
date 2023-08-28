974a9a07cc222eb75b50d71d988c52e58ffd04ec
Serpil-Dndr <serpil.dndrca@gmail.com>
2023-08-28 16:36:02 -0400

    added sout

--- src/test/java/stepdefinitions/gitPracticeAsil1.java
+++ src/test/java/stepdefinitions/gitPracticeAsil1.java
@@ -3,6 +3,7 @@ package stepdefinitions;
 public class gitPracticeAsil1 {
     public static void main(String[] args) {
         int asil = 33;
+        System.out.println(23344);
         String asil2 =" github practice";
     }
 }
